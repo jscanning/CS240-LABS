d284dd18ac385098b5094fc3f595599c187383ba
Jeremy Canning <jscanning@cpp.edu>
2017-02-08 21:51:25 -0800

    revision 3: InsertionSort

--- src/lab2/InsertionSort.java
+++ src/lab2/InsertionSort.java
@@ -1,5 +1,33 @@
 package lab2;
 
-public class InsertionSort {
+public class InsertionSort 
+{
+	public void interative(int[] array, int first, int last)
+	{
+		for(int unsorted = first + 1; unsorted <= last; unsorted++)
+		{
+			int nextToInsert = array[unsorted];
+			insertInOrder(nextToInsert, array, first, unsorted - 1);
+		}
+	}
+	
+	public void recursive(int[] array, int first, int last)
+	{
+		if(first < last)
+		{
+			recursive(array, first, last - 1);
+			insertInOrder(array[last], array, first, last - 1);
+		}
+	}
 
+	private void insertInOrder(int anEntry, int[] array, int begin, int end) 
+	{
+		int index = end;
+		while((index >= begin) && (anEntry < array[index]))
+		{
+			array[index+1] = array[index];
+			index--;
+		}
+		array[index+1] = anEntry;
+	}
 }
