7af23cde6b5f6d9660601fbaebc967e6b4fc2b94
Jeremy Canning <jscanning@cpp.edu>
2017-02-08 22:47:07 -0800

    revision 4: ShellSort iterative

--- src/lab2/ShellSort.java
+++ src/lab2/ShellSort.java
@@ -1,5 +1,38 @@
 package lab2;
 
-public class ShellSort {
-
+public class ShellSort 
+{
+	private void incrementalInsertionSort(int[] array, int first, int last, int increment)
+	{
+		for(int unsorted = first + increment; unsorted < last; unsorted += increment)
+		{
+			int nextToInsert = array[unsorted];
+			int index = unsorted - increment;
+			while((index >= first) && (nextToInsert > array[index]))
+			{
+				array[index + increment] = array[index];
+				index = index - increment;
+			}
+			array[index + increment] = nextToInsert;
+		}
+	}
+	
+	public void iterativeShellSort(int[] array, int first, int last)
+	{
+		int sequence = 1;
+		while(sequence < last)
+		{
+			for(int begin = first; begin <= first + sequence - 1; begin++)
+			{
+				incrementalInsertionSort(array, begin, last, sequence);
+			}
+			sequence = (2*sequence + 1);
+		}
+		incrementalInsertionSort(array, first, last, 1);
+	}
+	
+	public void recursiveShellSort(int[] array, int first, int last)
+	{
+		
+	}
 }
