2d37428b3d3a731582fb2c9a5a4b270e4df619c9
Jeremy Canning <jscanning@cpp.edu>
2017-02-09 21:36:04 -0800

    revision 5: QuickSort

--- src/lab2/QuickSort.java
+++ src/lab2/QuickSort.java
@@ -1,5 +1,87 @@
 package lab2;
 
-public class QuickSort {
+import java.util.Random;
 
+public class QuickSort 
+{
+	public void recursiveQuickSort(int[] array, int first, int last)
+	{
+		if(first < last)
+		{
+			int pivotIndex = partition(array, first, last);
+			recursiveQuickSort(array, first, pivotIndex - 1);
+			recursiveQuickSort(array, pivotIndex + 1, last);
+		}
+	}
+	
+	
+	private int partition(int[] array, int first, int last)
+	{
+		int pivotValue = array[last];
+		int index = (first-1);
+		for(int j = first; j < last; j++)
+		{
+			if(array[j] <= pivotValue)
+			{
+				index++;
+				swap(array, index, j);
+			}
+		}
+		
+		swap(array, index + 1, last);
+		return index + 1;
+	}
+	
+	public void iterativeQuickSort(int[] array, int first, int last)
+	{
+		int stack[] = new int[last - first + 1];
+		int top = -1;
+		stack[++top] = first;
+		stack[++top] = last;
+		
+		while(top >= 0)
+		{
+			last = stack[top--];
+			first = stack[top--];
+			
+			int pivot = partition(array, first, last);
+			
+			if(pivot - 1 > 1)
+			{
+				stack[++top] = first;
+				stack[++top] = pivot - 1;
+			}
+			
+			if(pivot + 1 < last)
+			{
+				stack[++top] = pivot + 1;
+				stack[++top] = last;
+			}
+		}
+	}
+	
+	public void printArray(int array[])
+	{
+		for(int i = 0; i < array.length; i++)
+			System.out.print(array[i] + " ");
+		System.out.println();
+	}
+	
+	private void swap(int[] array, int pos1, int pos2)
+	{
+		int temp;
+		temp = array[pos1];
+		array[pos1] = array[pos2];
+		array[pos2] = temp;
+	}
+	
+	public static void main(String args[])
+	{
+		QuickSort ob = new QuickSort();
+		Random rand = new Random();
+		int arr[] = {rand.nextInt(1000), rand.nextInt(1000), rand.nextInt(1000), rand.nextInt(1000), rand.nextInt(1000)};
+		ob.printArray(arr);
+		ob.recursiveQuickSort(arr, 0, arr.length-1);
+		ob.printArray(arr);
+	}
 }
