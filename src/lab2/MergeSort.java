bd9117b4f37a795905514d09e6344d98d307c05f
Jeremy Canning <jscanning@cpp.edu>
2017-02-12 13:31:12 -0800

    LAB2 revision 9: MergeSort functioning properly

--- src/lab2/MergeSort.java
+++ src/lab2/MergeSort.java
@@ -2,105 +2,94 @@ package lab2;
 
 public class MergeSort 
 {
-	int countMove = 0;
-	int countCompare = 0;
-	
-	public void iterative(int[] array)
+	static int countMove = 0;
+	static int countCompare = 0;
+
+	public void iterativeMergeSort(int[] a) 
 	{
-		for(int currentSize = 1; currentSize <= array.length; currentSize = 2*currentSize)
-		{
-			for(int leftStart = 1; leftStart < array.length; leftStart += 2*currentSize)
-			{
-				int middle = leftStart + currentSize - 1;
-				int rightEnd = findMinimum(leftStart + 2*currentSize - 1,  array.length);
-				
-				merge(array, leftStart, middle, rightEnd);
-			}
-		}
-	}
-	
-	public void recursive(int[] array, int left, int right)
-	{
-		if(left < right)
-		{
-			int mid = left+(right-1)/2;
-			recursive(array, left, mid);
-			recursive(array, mid+1, right);
-			merge(array, left, mid, right);
-		}
-	}
-	
-	private int findMinimum(int i, int j) 
-	{
-		if(i < j)
-			return i;
-		else
-			return j;
+		int[] aux = new int[a.length];
+		
+		for (int blockSize=1; blockSize<a.length; blockSize*=2)
+			for (int start=0; start<a.length; start+=2*blockSize)
+				merge(a, aux, start, start+blockSize, start+2*blockSize);
 	}
 
-	public void merge(int[] array, int left, int mid, int right)
+
+	public static void recursiveMergeSort(int[] a, int[] aux, int lo, int hi) 
 	{
-		int n1 = mid - left + 1;
-		int n2 = right - mid;
-		
-		int[] leftArr, rightArr;
-		leftArr = new int[n1];
-		rightArr = new int[n2];
-		
-		for(int i = 0; i < n1; i++)
-			leftArr[i] = array[left + i];
-		for(int j = 0; j < n2; j++)
-			rightArr[j] = array[mid + 1 + j];
-		
-		int i = 0;
-		int j = 0;
-		int k = 1;
-		while(i < n1 && j < n2)
+		// base case
+		if (hi - lo > 1) 
 		{
-			if(leftArr[i] <= rightArr[j])
-			{
-				array[k] = leftArr[i];
-				i++;
-			}
-			else
-			{
-				array[k] = rightArr[j];
-				j++;
-			}
-			k++;
-		}
-		
-		while(i < n1)
-		{
-			array[k] = leftArr[i];
-			i++;
-			k++;
-		}
-		
-		while(j < n2)
-		{
-			array[k] = rightArr[j];
-			j++;
-			k++;
+			// sort each half, recursively
+			int mid = lo + (hi - lo) / 2;
+			recursiveMergeSort(a, aux, lo, mid);
+			recursiveMergeSort(a, aux, mid, hi);
+			// merge back together
+			merge(a, aux, lo, mid, hi);
 		}
 	}
-	
+
+	public void recursiveMergeSort(int[] a) 
+	{
+		int n = a.length;
+		int[] aux = new int[n];
+		recursiveMergeSort(a, aux, 0, n);
+	}
+
+	private static void merge(int[] a, int[] aux, int begin, int mid, int end) 
+	{
+		if (mid < a.length) 
+		{
+			countCompare++;
+			if (end > a.length) 
+				end = a.length;
+			
+			int i = begin; 
+			int j = mid;
+			
+			countCompare++;
+			for (int k = begin; k < end; k++, countCompare++, countMove++) // compares k to end every loop
+			{
+				countCompare++; // incrementing before the comparison (i to mid)
+				if (i == mid)
+					aux[k] = a[j++];
+				// when this else/if block comes out false countCompare does not get added to. Other blocks account for this.
+				else if (j == end) { countCompare++; // only adds if true, when false next else block accounts for this comparison
+					aux[k] = a[i++];
+				}
+				
+				else if (a[j] < a[i]) { countCompare+=2; // adding this comparison and the previous.
+					aux[k] = a[j++];
+				}
+				
+				else   // no comparison made but since previous else/ifs only added when true must add those comparisons.
+				{	countCompare+=2; // adding previous two comparisons whose results were false.
+					aux[k] = a[i++];
+				}
+			}
+			
+			for (int k = begin; k < end; k++, countCompare++, countMove++)
+				a[k] = aux[k];
+		}
+	}
+
 	public void printArray(int[] array)
 	{
 		for(int i = 0; i < array.length; i++)
 			System.out.print(array[i] + " ");
 		System.out.println();
 	}
-	
+
 	public static void main(String args[])
 	{
 		MergeSort ob = new MergeSort();
 		arrayGenerator ag = new arrayGenerator();
-		int[] arr = ag.generateArray(1000, 1000);
+		int[] arr = ag.generateArray(10, 1000);
 		ob.printArray(arr);
-		ob.iterative(arr);
+		ob.iterativeMergeSort(arr);
 		ob.printArray(arr);
+		System.out.println("Comparisons: " + countCompare + " Moves: " + countMove);
 	}
-	
-	
+
+
 }
